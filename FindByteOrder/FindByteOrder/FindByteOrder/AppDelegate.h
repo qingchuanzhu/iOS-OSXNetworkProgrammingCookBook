04905692212f1dc634309f58b4f459ca41721e0e
Qingchuan Zhu <zqc@utexas.edu>
2018-02-23 15:59:49 -0800

    Project creation of Find byte order

---
+++ FindByteOrder/FindByteOrder/FindByteOrder/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FindByteOrder
+//
+//  Created by Qingchuan Zhu on 2/23/18.
+//  Copyright © 2018 Qingchuan Zhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FindByteOrder/FindByteOrder/FindByteOrder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FindByteOrder
+//
+//  Created by Qingchuan Zhu on 2/23/18.
+//  Copyright © 2018 Qingchuan Zhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
