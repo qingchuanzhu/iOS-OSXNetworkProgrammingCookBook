19f28ba1a43319765763806c79d353dd0d73280b
Qingchuan Zhu <zqc@utexas.edu>
2018-02-24 11:30:37 -0800

    Added the new work address store model class

---
+++ FindNetworkAddress/FindNetworkAddress/NetworkAddressStore.h
@@ -0,0 +1,13 @@
+//
+//  NetworkAddressStore.h
+//  FindNetworkAddress
+//
+//  Created by Qingchuan Zhu on 2/24/18.
+//  Copyright © 2018 Qingchuan Zhu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NetworkAddressStore : NSObject
+
+@end
---
+++ FindNetworkAddress/FindNetworkAddress/NetworkAddressStore.m
@@ -0,0 +1,13 @@
+//
+//  NetworkAddressStore.m
+//  FindNetworkAddress
+//
+//  Created by Qingchuan Zhu on 2/24/18.
+//  Copyright © 2018 Qingchuan Zhu. All rights reserved.
+//
+
+#import "NetworkAddressStore.h"
+
+@implementation NetworkAddressStore
+
+@end
