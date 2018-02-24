335f0ee2bc1755c1e36ebf033c53e1900c7e49d3
Qingchuan Zhu <zqc@utexas.edu>
2018-02-24 12:03:31 -0800

    Added retrieve IP address method

--- FindNetworkAddress/FindNetworkAddress/NetworkAddressStore.m
+++ FindNetworkAddress/FindNetworkAddress/NetworkAddressStore.m
@@ -7,7 +7,46 @@
 //
 
 #import "NetworkAddressStore.h"
+#include <ifaddrs.h>
+#include <netdb.h>
+#include <sys/socket.h>
 
 @implementation NetworkAddressStore
+
+struct ifaddrs *interface = NULL;
+int success = 0;
+
+char naddr [INET6_ADDRSTRLEN];
+char nmask [INET6_ADDRSTRLEN];
+char ngate [INET6_ADDRSTRLEN];
+
+- (instancetype)init{
+    self = [super init];
+    if (self) {
+        
+    }
+    return self;
+}
+
+- (void)retriveAddressList{
+    success = getifaddrs(&interface);
+    if (success) {
+        struct ifaddrs *temp_addrs;
+        for (temp_addrs = interface; temp_addrs != NULL; temp_addrs = temp_addrs->ifa_next) {
+            int ipVersion;
+            NSLog(@"**********************");
+            if (temp_addrs->ifa_addr->sa_family == AF_INET) {
+                NSLog(@"IPv4");
+                ipVersion = AF_INET;
+            } else if (temp_addrs->ifa_addr->sa_family == AF_INET6) {
+                NSLog(@"IPv6");
+                ipVersion = AF_INET6;
+            } else {
+                NSLog(@"Unknown version");
+                ipVersion = 0;
+            }
+        }
+    }
+}
 
 @end
