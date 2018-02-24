db452920389ef950299a4dda0bb393fc1a0bee45
Qingchuan Zhu <zqc@utexas.edu>
2018-02-23 16:27:24 -0800

    Added comments

--- FindByteOrder/FindByteOrder/FindByteOrder/ViewController.m
+++ FindByteOrder/FindByteOrder/FindByteOrder/ViewController.m
@@ -27,12 +27,17 @@
 
 - (EndianType)byteOrder{
     EndianType type = ENDIAN_UNKNOW;
+    /*
+     Using union, sNum and cNum shares the same memory space, thus cNum[0] corresponding to lower end of address while cNum[1] corresponds to higher end of address.
+     If cNum[0] == 1 && cNum[1] == 2, then, higer byte of data is stored at lower address, leading to Big-endian
+     */
     union{
         short sNum;
         char cNum[sizeof(short)];
     } un;
     
     un.sNum = 0x0102;
+    // short type contains 2 bytes(16 bits)
     if (sizeof(short) == 2) {
         if (un.cNum[0] == 1 && un.cNum[1] == 2) {
             type = ENDIAN_BIG;
