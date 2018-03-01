7cad08dfba0923cb714a1502aac66fd49e6acbaf
Qingchuan Zhu <zqc@utexas.edu>
2018-03-01 11:20:43 -0800

    Added AddressInfo class for address resolution

---
+++ FindNetworkAddress/AddressResolution/AddressInfo.h
@@ -0,0 +1,13 @@
+//
+//  AddressInfo.h
+//  FindNetworkAddress
+//
+//  Created by Qingchuan Zhu on 3/1/18.
+//  Copyright © 2018 Qingchuan Zhu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AddressInfo : NSObject
+
+@end
---
+++ FindNetworkAddress/AddressResolution/AddressInfo.m
@@ -0,0 +1,13 @@
+//
+//  AddressInfo.m
+//  FindNetworkAddress
+//
+//  Created by Qingchuan Zhu on 3/1/18.
+//  Copyright © 2018 Qingchuan Zhu. All rights reserved.
+//
+
+#import "AddressInfo.h"
+
+@implementation AddressInfo
+
+@end
